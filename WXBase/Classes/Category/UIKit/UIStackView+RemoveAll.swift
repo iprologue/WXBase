f6e0ff0c39129434870f266ff3cd3e8072296a6e
iPrologue <ipprologue@gmail.com>
2020-10-30 17:08:30 +0800

    Init

---
+++ WXBase/Classes/Category/UIKit/UIStackView+RemoveAll.swift
@@ -0,0 +1,26 @@
+//
+//  UIStackView+RemoveAll.swift
+//  FGBase
+//
+//  Created by xiaoyang on 2019/10/31.
+//
+
+import Foundation
+
+@available(iOS 9.0, *)
+extension UIStackView {
+
+    public func removeAllArrangedSubviews() {
+
+        let removedSubviews = arrangedSubviews.reduce([]) { (allSubviews, subview) -> [UIView] in
+            self.removeArrangedSubview(subview)
+            return allSubviews + [subview]
+        }
+
+        // Deactivate all constraints
+        NSLayoutConstraint.deactivate(removedSubviews.flatMap({ $0.constraints }))
+
+        // Remove the views from self
+        removedSubviews.forEach({ $0.removeFromSuperview() })
+    }
+}
